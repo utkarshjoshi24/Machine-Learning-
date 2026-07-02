10844d1680d058453c380f0dda342e6106fe2948
utkarsh <utkarsh.joshi.2423@gmail.com>
2026-07-02 18:44:08 +0530

    Used get post method with flask

---
+++ Flask/HTTP_methods/getpost.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method=="POST":
+        name = request.form["name"]
+        return f"Hello {name}"
+    return render_template("form.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
